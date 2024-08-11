95876d3eafe56c3f4d9358846fa4fda4b27ece5a
Adil Rahimov <adilrahimovnet@gmail.com>
2024-08-11 22:42:28 +0400

    Hey

---
+++ E-commerce/frontent/my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Nevbar } from './Components/Nevbar/Nevbar';
+
+function App() {
+  return (
+    <div>
+        <Nevbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ E-commerce/frontent/my-app/src/Components/Nevbar/Nevbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+export const Nevbar = () => {
+  return (
+    <div className='navbar'>
+        
+    </div>
+  )
+}
+
+export default App;
